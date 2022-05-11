aebc2a9a5fa9a73a8c59c74638415490fae0f5d9
Melissa Guachun <1212malka@gmail.com>
2022-05-11 15:36:23 -0400

    add two numbers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [total, setTotal] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adding Two Numbers</h1>
+      <div className="num-input">
+        <input onChange={(e) => setInput1(e.target.value)} type="number" placeholder="0" value={input1}/>
+        <input onChange={(e) => setInput2(e.target.value)} type="number" placeholder="0" value={input2}/>
+        <p>{Number(input1) + Number(input2)}</p>
+
+      </div>
+
+      <button onClick={() => {setTotal(Number(input1) + Number(input2))}}>Add Them!</button>
+      { <h2>{total}</h2>}
     </div>
   );
 }
